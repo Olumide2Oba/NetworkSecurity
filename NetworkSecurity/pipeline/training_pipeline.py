952f83a2ab53a65de355e3f86d1e026d5d7ce88d
Olumide2Oba <olumide2obanla@gmail.com>
2025-06-27 18:07:42 +0100

    Deployment Changes

--- NetworkSecurity/pipeline/training_pipeline.py
+++ NetworkSecurity/pipeline/training_pipeline.py
@@ -9,6 +9,10 @@ from NetworkSecurity.components.data_ingestion import DataIngestion
 from NetworkSecurity.components.data_transformation import DataTransformation
 from NetworkSecurity.components.model_trainer import ModelTrainer
 
+from NetworkSecurity.cloud.s3_syncer import S3Sync  
+from NetworkSecurity.constant.training_pipeline import TRAINING_BUCKET_NAME
+from NetworkSecurity.constant.training_pipeline import SAVED_MODEL_DIR
+
 from NetworkSecurity.entity.config_entity import (
     TrainingPipelineConfig,
     DataIngestionConfig,
@@ -27,6 +31,8 @@ from NetworkSecurity.entity.artifact_entity import (
 class TrainingPipeline:
     def __init__(self):
         self.training_pipeline_config = TrainingPipelineConfig()
+        self.s3_sync = S3Sync()
+        
     
     def start_data_ingestion(self):
         try:
@@ -62,6 +68,20 @@ class TrainingPipeline:
         except Exception as e:
             raise NetworkSecurityException(e,sys)
     
+    def sync_artifact_dir_to_s3(self):
+        try:
+            aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/Artifacts/{self.training_pipeline_config.timestamp}"
+            self.s3.sync.sync_folder_to_s3(folder = self.training_pipeline_config.artifact_dir,aws_bucket_url=aws_bucket_url)
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    def sync_saved_model_dir_to_s3(self):
+        try:
+            aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/final_model/{self.training_pipeline_config.timestamp}"
+            self.s3_sync.sync_folder_from_s3(folder = self.training_pipeline_config.model_dir,aws_bucket_url=aws_bucket_url)
+        except Exception as e: 
+            raise NetworkSecurityException(e,sys)
+    
     def start_model_trainer(self,data_transformation_artifact:DataTransformationArtifact) -> ModelTrainerArtifact:
         try:
             self.model_trainer_config:ModelTrainerConfig=ModelTrainerConfig(training_pipeline_config=self.training_pipeline_config)
@@ -78,6 +98,9 @@ class TrainingPipeline:
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
             data_transformation_artifact = self.start_data_transformation(data_validation_artifact=data_validation_artifact)
             model_tainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
+            self.sync_artifact_dir_to_s3()
+            self.sync_saved_model_dir_to_s3()            
+            
             return model_tainer_artifact
         
         except Exception as e:
